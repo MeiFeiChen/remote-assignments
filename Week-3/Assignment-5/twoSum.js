b627bd1302d1982373a2b076e58e22199f45547e
ameliachen <meifeiccc@gmail.com>
2023-09-17 22:30:54 +0800

    Add week-3 assignment

---
+++ Week-3/Assignment-5/twoSum.js
@@ -0,0 +1,36 @@
+// function twoSum(nums, target) {
+//     let remainValue;
+//     let checkIndex;
+//     for (let i = 0 ; i < nums.length; i++) {
+//         remainValue = target -  nums[i];
+//         if (nums.includes(remainValue, i+1)) {
+//             checkIndex = i + nums.slice(i+1).indexOf(remainValue) + 1
+//             // checkIndex = i + nums.subarray(i+1).indexOf(remainValue);
+//             return [i, checkIndex];
+//         }
+//     }
+// };
+
+
+/**
+ * Returns an array contains the indices of  two numbers that 
+ * can add up to target.
+ *
+ * @param {array} nums - The array of numbers.
+ * @return {array} An array contains the indices of two numbers.
+ */
+function twoSum(nums, target) {
+    const checkValue = {};  // store val-index pairs 
+
+    for (let i = 0 ; i < nums.length; i++) {
+        // Check if the complement val exists in checkValue
+        if (checkValue[target-nums[i]] !== undefined) {
+            return [checkValue[target-nums[i]], i];
+        } else {
+            // add current val and index to checkValue
+            checkValue[nums[i]] = i;
+        }
+    }
+};
+console.log(twoSum([2, 7, 11, 15], 9))
+console.log(twoSum([3, 3], 6))
---
+++ Week-3/Assignments/app.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const app = express();
+
+// static assets
+app.use(express.static('./public'))
+// parser
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(cookieParser());
+
+// pug engine
+app.set('view engine', 'pug');
+
+// home
+app.get('/', (req, res) => {
+    const name = req.cookies.username;
+    res.render('index', {name:name});
+})
+
+// data
+app.get('/data', (req, res) => {
+    const {number} = req.query;
+    if (number === undefined) {
+        res.status(401).send('Lack of Parameter');
+    } else if (isNaN(number)) {
+        res.status(401).send('Wrong Parameter');
+    } else {
+        // count total in O(1)
+        const num = Number(number)
+        let total = num * (num + 1) / 2;
+        res.status(200).send(`${total}`);
+    }
+})
+
+// myName
+app.get('/myName', (req, res) => {
+    const name = req.cookies.username;
+    res.status(200).render('myName', {name});    
+})
+
+app.post('/myName', (req, res) => {
+    res.clearCookie('username');
+    res.status(200).render('myName')
+})
+
+// trackName
+app.get('/trackName', (req, res) => {
+    res.cookie('username', req.query.username)
+    res.redirect('myName')
+})
+
+app.listen(3000, () => {
+    console.log('The application is running on localhost:3000!')
+});
